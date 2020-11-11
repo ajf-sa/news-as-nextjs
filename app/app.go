3493a274b765c4ec6df91609606421b1c568a458
abdulaziz jaiz <abajall@gmail.com>
2020-11-12 00:03:09 +0300

    .

--- app/app.go
+++ app/app.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
-
+	app.Get("/login", func(ctx *fiber.Ctx) error {
+		cookie := new(fiber.Cookie)
+		cookie.Name = "test-fiber"
+		cookie.Value = "test-test-test"
+		cookie.Expires = time.Now().Add(24 * time.Hour)
+		ctx.Cookie(cookie)
+		return ctx.SendString(cookie.Value)
+	})
+	app.Get("/private", func(ctx *fiber.Ctx) error {
+		cookie := ctx.Cookies("test-fiber")
+		return ctx.SendString(cookie)
+	})
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hi")
 	})
