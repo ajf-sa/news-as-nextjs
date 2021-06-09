809b5bf7486f051b3075697043e44b478ba26420
abdulaziz jaiz <abajall@gmail.com>
2021-06-10 02:25:21 +0300

    .

--- app/pages/c/[slug].tsx
+++ app/pages/c/[slug].tsx
@@ -1,46 +1,39 @@
-import axios from 'axios'
-import Post from 'components/Post'
+import axios from 'axios';
+import Post from 'components/Post';
 
-const Category = ({tag}) =>{
-    
-  if(tag.length > 0){
+const Category = ({ tag }) => {
+  if (tag.length > 0) {
     return (
       <>
-      {tag?.map(t =>{
-        return(
-        t.posts?.map(p =>{
-          return (
-            <Post 
+        {tag?.map((t) => {
+          return t.posts?.map((p) => {
+            return (
+              <Post
                 key={p.id.toString()}
                 id={p.id}
                 title={p.title}
                 description={p.description}
-                tags={tag}
                 created_at={p.created_at}
-                image = {p.image}
-                />
-          )
-        })
-        )
-      })}
+                image={p.image}
+              />
+            );
+          });
+        })}
       </>
-    )
+    );
   }
-
-}
-
-
+};
 
 export async function getServerSideProps(context) {
-    const {slug} = context.query
-    const {APP_URL} = process.env
-    const res = await axios(`${APP_URL}/tags?slug=${slug}`)
-    const data = await res.data
-    console.log(slug);
-    
-    return {
-      props: {tag:data},
-        }
-  }
-  
-export default Category
+  const { slug } = context.query;
+  const { APP_URL } = process.env;
+  const res = await axios(`${APP_URL}/tags?slug=${slug}`);
+  const data = await res.data;
+  console.log(slug);
+
+  return {
+    props: { tag: data },
+  };
+}
+
+export default Category;
--- app/pages/post/[id].tsx
+++ app/pages/post/[id].tsx
@@ -1,31 +1,26 @@
-import axios from 'axios'
-import { PostType } from 'lib/interface'
-import Post from 'components/Post'
-export default function getPostById({post}){
-
-    return (
-        <>
-        <Post key={post.id}
-                        id={post.id}
-                        title={post.title}
-                        description={post.description}
-                        tags={post.tags}
-                        created_at ={post.created_at}
-                        image = {post.image}
-                        />
-        </>
-    )
+import axios from 'axios';
+import { PostType } from 'lib/interface';
+import Post from 'components/Post';
+export default function getPostById({ post }) {
+  return (
+    <>
+      <Post
+        key={post.id}
+        id={post.id}
+        title={post.title}
+        description={post.description}
+        created_at={post.created_at}
+        image={post.image}
+      />
+    </>
+  );
 }
 
-
 export async function getServerSideProps(context) {
-    const {APP_URL} = process.env
-    const res = await axios(`${APP_URL}/posts/${context.query.id}`)
-    const data:PostType = res.data
-    return {
-      props: {post:data}, // will be passed to the page component as props
-    }
-  }
-
-  
-  
+  const { APP_URL } = process.env;
+  const res = await axios(`${APP_URL}/posts/${context.query.id}`);
+  const data: PostType = res.data;
+  return {
+    props: { post: data }, // will be passed to the page component as props
+  };
+}
